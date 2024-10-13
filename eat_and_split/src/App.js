b861731e02f9e0d996f4d5ad2c7d1aea5a9b55ab
Dmitry Novikov <dredfort.42@gmail.com>
2024-10-13 22:28:45 +0100

    some information and examples about how react render scenes and update dom

--- eat_and_split/src/App.js
+++ eat_and_split/src/App.js
@@ -72,6 +72,7 @@ export default function App() {
                 <FormSplitBill
                     selectedFriend={selectedFriend}
                     onSubmit={handleSplitBill}
+                    key={selectedFriend.id}
                 />
             )}
         </div>
