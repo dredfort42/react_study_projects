486c35d50e5713817716c0d61496fc7650c88fbc
Dmitry Novikov <dredfort.42@gmail.com>
2024-10-26 17:35:28 +0100

    first useReducer hook

---
+++ react_quize/src/DateCounter.js
@@ -0,0 +1,84 @@
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "inc":
+      return state + action.payload;
+    case "dec":
+      return state - action.payload;
+    case "setCount":
+      return action.payload;
+
+    default:
+      return state;
+  }
+}
+
+function DateCounter() {
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+  const [step, setStep] = useState(1);
+
+  // This mutates the date object.
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const dec = function () {
+    // setCount((count) => count - 1);
+    // setCount((count) => count - step);
+
+    dispatch({ type: "dec", payload: step });
+  };
+
+  const inc = function () {
+    // setCount((count) => count + 1);
+    // setCount((count) => count + step);
+
+    dispatch({ type: "inc", payload: step });
+  };
+
+  const defineCount = function (e) {
+    // setCount(Number(e.target.value));
+
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+  };
+
+  const defineStep = function (e) {
+    setStep(Number(e.target.value));
+  };
+
+  const reset = function () {
+    // setCount(0);
+    dispatch({ type: "setCount", payload: 0 });
+    setStep(1);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
