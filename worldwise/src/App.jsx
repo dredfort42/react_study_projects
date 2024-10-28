3a560f05bf14e694b85b5a58c163ab15093d19c9
Dmitry Novikov <dredfort.42@gmail.com>
2024-10-28 22:07:47 +0000

    css modules and react router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
-import Home from './pages/Home';
+import Homepage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Homepage />} />
                 <Route path="product" element={<Product />} />
                 <Route path="pricing" element={<Pricing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="app" element={<AppLayout />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
