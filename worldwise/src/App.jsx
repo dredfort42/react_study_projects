4e788896d6268967e60dcbc980543e0f94afed35
Dmitry Novikov <dredfort.42@gmail.com>
2024-10-29 21:07:23 +0000

    nested routes

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -6,16 +7,54 @@ import Homepage from './pages/Homepage';
 import AppLayout from './pages/AppLayout';
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+
+const API_URL = 'http://localhost:5000';
 
 export default function App() {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function () {
+        async function fetchCities() {
+            setIsLoading(true);
+            try {
+                const res = await fetch(`${API_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchCities();
+    }, []);
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage />} />
+                <Route index element={<Homepage />} />
                 <Route path="product" element={<Product />} />
                 <Route path="pricing" element={<Pricing />} />
                 <Route path="login" element={<Login />} />
-                <Route path="app" element={<AppLayout />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route
+                        index
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route
+                        path="cities"
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route path="countries" element={<p>Countries list</p>} />
+                    <Route path="form" element={<p>Form</p>} />
+                </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
--- worldwise/src/components/AppNav.jsx
+++ worldwise/src/components/AppNav.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import style from './AppNav.module.css';
 
 export default function AppNav() {
-    return <nav className={style.nav}>AppNav</nav>;
+    return (
+        <nav className={style.nav}>
+            <ul>
+                <li>
+                    <NavLink to="cities">Cities</NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">Countries</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
