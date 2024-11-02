8dd5b47ad42dcbfd2357cf2649e0ebdc1cba05e7
Dmitry Novikov <dredfort.42@gmail.com>
2024-11-02 15:32:53 +0000

    useNavigate - navigate added

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,20 +1,21 @@
-import { useSearchParams } from 'react-router-dom';
-import styles from './Map.module.css';
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 export default function Map() {
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
-    return (
-        <div className={styles.mapContainer}>
-            <h1>Map</h1>
-            <h2>
-                Position: {lat} {lng}
-            </h2>
-            <button onClick={() => setSearchParams({ lat: 21, lng: 42 })}>
-                Set new place
-            </button>
-        </div>
-    );
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat} {lng}
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 21, lng: 42 })}>
+        Set new place
+      </button>
+    </div>
+  );
 }
