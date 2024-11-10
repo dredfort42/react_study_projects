4a6b1cb5a51321673312fc28b507b219c9d67587
Dmitry Novikov <dredfort.42@gmail.com>
2024-11-10 22:02:49 +0000

    performance optimization using memo and useMemo

--- atomic_blog/src/index.js
+++ atomic_blog/src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./style.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App_memo';
+import { PostProvider } from './PostProvider';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <PostProvider>
+            <App />
+        </PostProvider>
+    </React.StrictMode>
 );
