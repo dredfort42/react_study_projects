1390b8755e721b964af476e483b921be7909babd
Dmitry Novikov <dredfort.42@gmail.com>
2024-11-16 19:09:52 +0000

    redux store and reducer

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,7 +1,9 @@
+import { createStore } from 'redux';
+
 const initialState = {
     balance: 0,
     loan: 0,
-    loadPurpose: '',
+    loanPurpose: '',
 };
 
 function reducer(state = initialState, action) {
@@ -20,19 +22,43 @@ function reducer(state = initialState, action) {
             if (state.loan > 0) return state;
             return {
                 ...state,
-                balance: state.balance + action.payload,
+                balance: state.balance + action.payload.amount,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose,
             };
         case 'account/payLoan':
-            const pay =
-                state.loan > action.payload ? state.loan : action.payload;
+            if (action.payload <= 0) return state;
+            let pay =
+                state.balance > action.payload ? action.payload : state.balance;
+            pay = state.loan > pay ? pay : state.loan;
+
             return {
                 ...state,
                 balance: state.balance - pay,
                 loan: state.loan - pay,
-                loanPurpose: '',
+                loanPurpose: state.loan - pay === 0 ? '' : state.loanPurpose,
             };
 
         default:
             return state;
     }
 }
+
+const store = createStore(reducer);
+store.dispatch({ type: 'account/deposit', payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: 'account/withdraw', payload: 200 });
+console.log(store.getState());
+store.dispatch({
+    type: 'account/requestLoan',
+    payload: { amount: 10000, purpose: 'buy a car' },
+});
+console.log(store.getState());
+store.dispatch({ type: 'account/payLoan', payload: 5000 });
+console.log(store.getState());
+store.dispatch({ type: 'account/payLoan', payload: 5000 });
+console.log(store.getState());
+store.dispatch({ type: 'account/payLoan', payload: 5000 });
+console.log(store.getState());
+store.dispatch({ type: 'account/payLoan', payload: -5000 });
+console.log(store.getState());
