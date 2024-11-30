d6caa2fee28b9f2e68af2f346e5317cda2788089
Dmitry Novikov <dredfort.42@gmail.com>
2024-11-30 20:19:17 +0000

    tailwind classes

--- fast_react_pizza/src/features/cart/Cart.jsx
+++ fast_react_pizza/src/features/cart/Cart.jsx
@@ -29,7 +29,10 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu" className="text-sm text-blue-900 hover:text-blue-500">
+      <Link
+        to="/menu"
+        className="text-sm text-blue-900 hover:text-blue-500 hover:underline"
+      >
         &larr; Back to menu
       </Link>
 
--- fast_react_pizza/src/features/order/CreateOrder.jsx
+++ fast_react_pizza/src/features/order/CreateOrder.jsx
@@ -48,13 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,12 +62,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
@@ -79,7 +80,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-500 focus:bg-yellow-500 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
@@ -106,7 +110,9 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+
+  return null;
 }
