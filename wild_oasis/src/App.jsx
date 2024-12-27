63fbe93d8230c54513e9272a614fca0bb226fdc6
Dmitry Novikov <dredfort.42@gmail.com>
2024-12-27 20:20:39 +0000

    ready to build the app layout

--- wild_oasis/src/App.jsx
+++ wild_oasis/src/App.jsx
@@ -1,37 +1,36 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import GlobalStyles from '../src/styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
 
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow;
-`;
+import Dashboard from '../src/pages/Dashboard';
+import Booking from '../src/pages/Bookings';
+import Account from '../src/pages/Account';
+import Cabins from '../src/pages/Cabins';
+import Login from '../src/pages/Login';
+import PageNotFound from '../src/pages/PageNotFound';
+import Settings from '../src/pages/Settings';
+import Users from '../src/pages/Users';
 
-const StyledApp = styled.div`
-    background-color: orangered;
-    padding: 20pxs;
-`;
-
-function App() {
+export default function App() {
     return (
         <>
             <GlobalStyles />
-            <StyledApp>
-                <H1>The Wild Oasis</H1>
-
-                <Button onClick={() => alert('Clicked 0')}>
-                    Test button 0
-                </Button>
-                <Button onClick={() => alert('Clicked 1')}>
-                    Test button 1
-                </Button>
-                <Input type="number" placeholder="Number of guests" />
-                <Input type="number" placeholder="Number of guests 1" />
-            </StyledApp>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        index
+                        element={<Navigate replace to="dashboard" />}
+                    />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="booking" element={<Booking />} />
+                    <Route path="account" element={<Account />} />
+                    <Route path="cabins" element={<Cabins />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="settings" element={<Settings />} />
+                    <Route path="users" element={<Users />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
-
-export default App;
