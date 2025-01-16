a1ca4f8976a5589301c1d39d7d850d8bd16edab8
Dmitry Novikov <dredfort.42@gmail.com>
2025-01-16 21:20:08 +0000

    filter and sort bookings on server side

--- wild_oasis/src/services/apiBookings.js
+++ wild_oasis/src/services/apiBookings.js
@@ -1,12 +1,24 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-    const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+    let query = supabase
         .from('bookings')
         .select(
             'id, created_at, start_date, end_date, number_of_nights, number_of_guests, status, total_price, cabins(name), guests(full_name, email)'
         );
+
+    if (filter) {
+        query = query[filter.method || 'eq'](filter.field, filter.value);
+    }
+
+    if (sortBy) {
+        query = query.order(sortBy.field, {
+            ascending: sortBy.direction === 'asc',
+        });
+    }
+
+    const { data, error } = await query;
 
     if (error) {
         console.log(error);
--- wild_oasis/src/ui/SortBy.jsx
+++ wild_oasis/src/ui/SortBy.jsx
@@ -3,10 +3,10 @@ import Select from './Select';
 
 export default function SortBy({ options }) {
     const [searchParams, setSearchParams] = useSearchParams();
-    const sortBy = searchParams.get('sortBy') || '';
+    const sortBy = searchParams.get('sort_by') || '';
 
     function handleChange(e) {
-        searchParams.set('sortBy', e.target.value);
+        searchParams.set('sort_by', e.target.value);
         setSearchParams(searchParams);
     }
 
