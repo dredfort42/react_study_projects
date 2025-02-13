31a2273c66a8a1342f2aed24f5a107cc8f867bc0
Dmitry Novikov <dredfort.42@gmail.com>
2025-02-13 19:52:28 +0000

    prepare data for statistics on dashboard

--- wild_oasis/src/pages/Dashboard.jsx
+++ wild_oasis/src/pages/Dashboard.jsx
@@ -1,13 +1,34 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import DashboardFilter from '../features/dashboard/DashboardFilter';
+import DashboardLayout from '../features/dashboard/DashboardLayout';
+import { useRecentBookings } from '../features/dashboard/useRecentBookings';
+import { useRecentStays } from '../features/dashboard/useRecentStays';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import Spinner from '../ui/Spinner';
 
 function Dashboard() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    const { bookings, isPending: isBookingsLoading } = useRecentBookings();
+    const {
+        stays,
+        confirmedStays,
+        isPending: isStaysLoading,
+    } = useRecentStays();
+
+    if (isBookingsLoading || isStaysLoading) return <Spinner />;
+
+    console.log(bookings);
+    console.log(stays, confirmedStays);
+
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">Dashboard</Heading>
+                <DashboardFilter />
+            </Row>
+
+            <DashboardLayout />
+        </>
+    );
 }
 
 export default Dashboard;
