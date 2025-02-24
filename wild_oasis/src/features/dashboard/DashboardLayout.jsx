cd943dfe7d1572386c0e620d906de6486f1e0ccb
Dmitry Novikov <dredfort.42@gmail.com>
2025-02-24 22:38:36 +0000

    Pie chart

--- wild_oasis/src/features/dashboard/DashboardLayout.jsx
+++ wild_oasis/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from '../../features/cabins/useCabins';
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -38,7 +39,7 @@ export default function DashboardLayout() {
                 cabinCount={cabins.length}
             />
             <div>Today's activity</div>
-            <div>Chart stay duration</div>
+            <DurationChart confirmedStays={confirmedStays} />
             <SalesChart bookings={bookings} numDays={numDays} />
         </StyledDashboardLayout>
     );
