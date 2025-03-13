98808aea842bd78523576d5e850d8b5f53de0c94
Dmitry Novikov <dredfort.42@gmail.com>
2025-03-13 20:35:27 +0000

    async fetch data on server side

---
+++ wild_oasis_website/app/layout.js
@@ -0,0 +1,18 @@
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                <header>
+                    <Logo />
+                    <Navigation />
+                </header>
+                <main>{children}</main>
+                <footer>Copyright by dredfort42</footer>
+            </body>
+        </html>
+    );
+}
+
---
+++ wild_oasis_website/app/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'The Wild Oasys',
+};
+
+export default function Home() {
+    return <h1>Hello Next.js 15!</h1>;
+}
