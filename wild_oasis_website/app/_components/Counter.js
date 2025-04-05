6eb97c11897b1cca5a9107c5328ac9671a184dee
Dmitry Novikov <dredfort.42@gmail.com>
2025-04-05 21:05:31 +0100

    project to restart from scratch

--- wild_oasis_website/app/_components/Counter.js
+++ wild_oasis_website/app/_components/Counter.js
@@ -1,13 +1,14 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Counter({ users }) {
-    const [count, setCount] = useState(0);
-    return (
-        <>
-            <button onClick={() => setCount((c) => c + 1)}>{count}</button>
-            <p>There are {users.length} users</p>
-        </>
-    );
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>There are {users.length} users</p>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+    </div>
+  );
 }
--- wild_oasis_website/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-    title: 'The Wild Oasys',
-};
-
-export default function Home() {
-    return <h1>Hello Next.js 15!</h1>;
-}
