28ab5d241c106b05a4189f8069ed09bfe1a11134
Dmitry Novikov <dredfort.42@gmail.com>
2025-07-06 21:46:22 +0100

    usePathname hook

--- wild_oasis_website/app/_components/SideNavigation.js
+++ wild_oasis_website/app/_components/SideNavigation.js
@@ -1,51 +1,58 @@
-import Link from "next/link";
+'use client';
+
+import Link from 'next/link';
 import {
-  CalendarDaysIcon,
-  HomeIcon,
-  UserIcon,
-} from "@heroicons/react/24/solid";
-import SignOutButton from "./SignOutButton";
+    CalendarDaysIcon,
+    HomeIcon,
+    UserIcon,
+} from '@heroicons/react/24/solid';
+import SignOutButton from './SignOutButton';
+import { usePathname } from 'next/navigation';
 
 const navLinks = [
-  {
-    name: "Home",
-    href: "/account",
-    icon: <HomeIcon className="h-5 w-5 text-primary-600" />,
-  },
-  {
-    name: "Reservations",
-    href: "/account/reservations",
-    icon: <CalendarDaysIcon className="h-5 w-5 text-primary-600" />,
-  },
-  {
-    name: "Guest profile",
-    href: "/account/profile",
-    icon: <UserIcon className="h-5 w-5 text-primary-600" />,
-  },
+    {
+        name: 'Home',
+        href: '/account',
+        icon: <HomeIcon className="h-5 w-5 text-primary-600" />,
+    },
+    {
+        name: 'Reservations',
+        href: '/account/reservations',
+        icon: <CalendarDaysIcon className="h-5 w-5 text-primary-600" />,
+    },
+    {
+        name: 'Guest profile',
+        href: '/account/profile',
+        icon: <UserIcon className="h-5 w-5 text-primary-600" />,
+    },
 ];
 
 function SideNavigation() {
-  return (
-    <nav className="border-r border-primary-900">
-      <ul className="flex flex-col gap-2 h-full text-lg">
-        {navLinks.map((link) => (
-          <li key={link.name}>
-            <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
-              href={link.href}
-            >
-              {link.icon}
-              <span>{link.name}</span>
-            </Link>
-          </li>
-        ))}
+    const pathname = usePathname();
 
-        <li className="mt-auto">
-          <SignOutButton />
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="border-r border-primary-900">
+            <ul className="flex flex-col gap-2 h-full text-lg">
+                {navLinks.map((link) => (
+                    <li key={link.name}>
+                        <Link
+                            className={`py-3 px-5 hover:bg-gray-100 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                                pathname === link.href ? 'bg-gray-100' : ''
+                            }`}
+                            href={link.href}
+                        >
+                            {link.icon}
+                            <span>{link.name}</span>
+                        </Link>
+                    </li>
+                ))}
+
+                <li className="mt-auto">
+                    <SignOutButton />
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default SideNavigation;
